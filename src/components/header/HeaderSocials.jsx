82b231b40c5945db1d3a3aa56b60d16741c1ffee
silvakeci1 <silvakeci.ing@gmail.com>
2023-08-22 17:18:38 +0200

    changes

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocial=()=>{
+    return (
+        <div className='header__socials'>
+            <a herf="https://linkedin.com" target="_blank">Scroll Down</a>
+            <a herf="https://github.com" target="_blank">Scroll Down</a>
+            <a herf="https://dribble.com" target="_blank">Scroll Down</a>
+        </div>
+    )
+}
+export default HeaderSocial;
