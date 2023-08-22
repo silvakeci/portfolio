0986638424911cf98a1eb308e473ada3df79b30a
silva <silvakeci.ing@gmail.com>
2023-08-22 22:43:26 +0300

    changes

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-
-const HeaderSocial=()=>{
-    return (
-        <div className='header__socials'>
-            <a herf="https://linkedin.com" target="_blank">Scroll Down</a>
-            <a herf="https://github.com" target="_blank">Scroll Down</a>
-            <a herf="https://dribble.com" target="_blank">Scroll Down</a>
-        </div>
-    )
-}
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsDribbble } from "react-icons/bs";
+const HeaderSocial = () => {
+  return (
+    <div className="header__socials">
+      <a herf="https://linkedin.com" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a herf="https://github.com" target="_blank">
+        <BsGithub />
+      </a>
+      <a herf="https://dribble.com" target="_blank">
+        <BsDribbble />
+      </a>
+    </div>
+  );
+};
 export default HeaderSocial;
