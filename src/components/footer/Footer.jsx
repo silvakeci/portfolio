b407718a91c9bbb02707b84a8713e1beee7f77a7
silva <silvakeci.ing@gmail.com>
2023-08-24 23:40:51 +0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import './footer.css'
-const Footer=()=>{
-    return (
-        <section id='footer'>Footer</section>
-    )
-}
+import React from "react";
+import "./footer.css";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { PiTwitterLogoBold } from "react-icons/pi";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        EGATOR
+      </a>
+      <ul className="permalinks">
+        <li href="#">Home</li>
+        <li href="#about">About</li>
+        <li href="#expeirience">Expeirience</li>
+        <li href="#services">Services</li>
+        <li href="#portfolio">Portfolio</li>
+        <li href="#testimonials">Testimonials</li>
+        <li href="#contact">Contact</li>
+      </ul>
+      <div className="footer__socials">
+        <a href="www.instagram.com">
+          <AiOutlineInstagram />
+        </a>
+        <a href="www.facebook.com">
+          <AiOutlineFacebook />
+        </a>
+        <a href="www.twitter.com">
+          <PiTwitterLogoBold />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
