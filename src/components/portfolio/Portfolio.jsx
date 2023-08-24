f8e2e8a4c45f6aa838e074dc4907bd43b5f56bb4
silvakeci1 <silvakeci.ing@gmail.com>
2023-08-24 11:01:58 +0200

    commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import './portfolio.css'
 const Portfolio=()=>{
     return (
-        <section id='portfolio'>Portfolio</section>
+        <section id='portfolio'>
+            <h5>Me Recent Work</h5>
+            <h2>Portfolio</h2>
+            <div className=" container portfolio__container">
+                <article className="portfolio_item">
+                    <div className="portfolio_item-image">
+                        <h3>This is portfolio item title</h3>
+                        <a href="" className='btn'>GitHub</a>
+                        <a href="" className='btn btn-primary'>live demo</a>
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 }
 
