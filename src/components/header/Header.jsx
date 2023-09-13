00ce5007e61489deefc00a28d5c1493d58c077b6
silvakeci1 <silvakeci.ing@gmail.com>
2023-09-13 10:51:27 +0200

    changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Silva Keci</h1>
-        <h5 className="text-light">Forntend Developer</h5>
+        <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocial />
         <div className="me">
